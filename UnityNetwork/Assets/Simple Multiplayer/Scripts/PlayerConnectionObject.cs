56cd099d951f0b7f0c19e210f98ba092a0765ea6
Nguyen Duong <ducnguyenduchao@gmail.com>
2018-01-24 23:13:09 -0800

    Added: gitignore file, my own project to the repository. Updated: project directory in order for the gitignore to work

---
+++ UnityNetwork/Assets/Simple Multiplayer/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour
+{
+
+    public GameObject PlayerUnitPrefab;
+
+    // SyncVar are variables where if their value changes on the server,
+    // then all clients are automatically informed of the new value.
+    [SyncVar]
+    public string PlayerName = "Default";
+
+    private GameObject PlayerUnit;
+
+    void Start()
+    {
+        // Is this my own local object
+        if (!isLocalPlayer)
+        {
+            // this objecct belong to other player
+            return;
+        }
+
+        // Spawn an object to the world
+        CmdSpawnMyUnit();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("number of connections: " + Network.connections.Length);
+    }
+
+
+    // COMMANDS
+    // Commande are specials functions that ONLY get executed on the server
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        Debug.Log("PlayerObject: I am alive !!!");
+        PlayerUnit = Instantiate(PlayerUnitPrefab);
+
+        PlayerUnit.GetComponent<PlayerUnit>().SetConnectionObject(this);
+        NetworkServer.SpawnWithClientAuthority(PlayerUnit, connectionToClient);
+        CmdChangePlayerName(PlayerName);
+    }
+
+
+    [Command]
+    void CmdChangePlayerName(string name)
+    {
+        PlayerName = name + Random.Range(1,100);
+        PlayerUnit.transform.Find("name").GetComponent<TextMesh>().text = PlayerName;
+        Debug.Log("name changed");
+    }
+
+
+    ///////////////RPC
+    // RPCs are special functions that ONLY get exrcuted on the clients
+
+    //[ClientRpc]
+    //void RpcChangePlayerName(string name)
+    //{
+    //    PlayerName = name;
+    //}
+}
---
+++ UnityNetwork/Assets/Simple Multiplayer/Scripts/PlayerUnit.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerUnit : NetworkBehaviour {
+
+    private PlayerConnectionObject ConnectionObject;
+    public float speed = 10;
+
+	void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log(Network.connections.Length);
+        //Debug.Log("co " + ConnectionObject.PlayerName);
+        // this.transform.Find("name").GetComponent<TextMesh>().text;
+
+        if (!hasAuthority)
+        {
+            return;
+        }
+        // Debug.Log(hasAuthority);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)
+           || Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Im movingg");
+        }
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed,
+                              0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+#if UNITY_ANDROID || IOS
+        if(Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if(Input.GetTouch(0).position.x > Screen.width / 2)
+            {
+                transform.Translate(1 * Time.deltaTime * speed, 0, 0);
+            }else if(Input.GetTouch(0).position.x <= Screen.width / 2)
+            {
+                transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+            }else if (Input.GetTouch(0).position.y > Screen.height / 2)
+            {
+                transform.Translate(0, 0, 1 * Time.deltaTime * speed);
+            }
+            else if (Input.GetTouch(0).position.y < Screen.height / 2)
+            {
+                transform.Translate(0, 0, -1 * Time.deltaTime * speed);
+            }
+        }
+#endif
+    }
+
+    public void SetConnectionObject(PlayerConnectionObject co)
+    {
+        ConnectionObject = co;
+    }
+
+
+}
